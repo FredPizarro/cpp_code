f96739854feb038bfd95d493b5414623e26c7936
FredPizarro <freddy_ps_3@hotmail.com>
2023-02-11 16:40:37 -0500

    Mi primer codigo

---
+++ 001_Mi-primer-codigo.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream> // importamos modulo/libreria
+
+using namespace std; // evitamos el prefijo     std::
+// int: numero entero corto
+int main(){ //funcion main() sin parametros,
+            // que retorna un numero entero
+    cout << "Hello, world"; // console out
+    // std estandard (standard)
+    return 0; // (defecto) finaliza la funcion
+}
